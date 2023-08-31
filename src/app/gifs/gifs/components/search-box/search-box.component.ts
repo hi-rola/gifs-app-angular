2021bcaf815b0f7ef565854f211bdc9482de2bd2
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-08-30 19:35:53 -0600

    inicio de proyecto gifs

---
+++ src/app/gifs/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs"
+    />
+  `,
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) {}
+
+  /* searchTag(newTag: string) { */
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+}
